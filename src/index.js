41ddeef25783e6cee87131ddfcebdd0b51728516
azaz2025 <azazahmaddb2023@gmail.com>
2026-09-03 10:21:41 +0530

    added playlist functionality

--- src/index.js
+++ src/index.js
@@ -7,10 +7,7 @@ import authRoutes from "./routes/auth.routes.js";
 import problemRoutes from "./routes/problem.routes.js"; 
 import executionRoute from "./routes/executeCode.routes.js";
 import submissionRoutes from "./routes/submission.routes.js";
-
-// import executionRoute from "./routes/executeCode.routes.js";
-// import submissionRoutes from "./routes/submission.routes.js";
-//     import playlistRoutes from "./routes/playlist.routes.js";
+import playlistRoutes from "./routes/playlist.routes.js";
 
 dotenv.config();
 
@@ -33,8 +30,7 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/problems", problemRoutes);
 app.use("/api/v1/execute-code",executionRoute );
 app.use("/api/v1/submission", submissionRoutes);
-
-// app.use("/api/v1/playlist", playlistRoutes);
+app.use("/api/v1/playlist", playlistRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running on port 8080");
